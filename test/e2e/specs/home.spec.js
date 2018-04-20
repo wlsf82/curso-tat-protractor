3b2aa5a3dc2f4e764c7a8bae11b083f15e9c0e8d
wlsf82 <wlsf82@gmail.com>
2018-04-21 01:11:43 +0200

    make test step one line of code

--- test/e2e/specs/home.spec.js
+++ test/e2e/specs/home.spec.js
@@ -9,11 +9,7 @@ describe("given I'm at the home page", () => {
 
     describe("when I click the 'add new item' button", () => {
         it("then I'm redirected to the relative URL 'items/create'", () => {
-            helper.clickWhenClickable(
-                home
-                    .navigation
-                    .addNewItemButton
-            );
+            helper.clickWhenClickable(home.navigation.addNewItemButton);
 
             helper.waitForUrlToBeEqualToExpectedUrl(`${browser.baseUrl}items/create`);
         });

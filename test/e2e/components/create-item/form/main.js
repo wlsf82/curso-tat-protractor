a0263aae084b089fa93000bc15b356de6acae3a6
wlsf82 <wlsf82@gmail.com>
2018-04-23 01:48:14 +0200

    add method on form component
    Method to fill the form with provided data and submit.

--- test/e2e/components/create-item/form/main.js
+++ test/e2e/components/create-item/form/main.js
@@ -1,3 +1,5 @@
+const helper = require("protractor-helper");
+
 const FormComponent = require("./form");
 const NavigationComponent = require("./navigation");
 
@@ -7,6 +9,22 @@ class FormContainer {
 
         this.form = new FormComponent(this.container);
         this.navigation = new NavigationComponent(this.container);
+    }
+
+    fillFormWithDataAndSubmit(data) {
+        helper.fillFieldWithTextWhenVisible(
+            this.form.titleField,
+            data.title
+        );
+        helper.fillFieldWithTextWhenVisible(
+            this.form.descriptionField,
+            data.description
+        );
+        helper.fillFieldWithTextWhenVisible(
+            this.form.imageUrlField,
+            data.imageUrl
+        );
+        helper.clickWhenClickable(this.navigation.createButton);
     }
 }
 

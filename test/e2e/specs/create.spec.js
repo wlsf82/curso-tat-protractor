bb8caaffb5ea35c295ab5fdbbf141b6bf13b0fc0
wlsf82 <wlsf82@gmail.com>
2018-04-23 18:37:13 +0200

    shorten test file

--- test/e2e/specs/create.spec.js
+++ test/e2e/specs/create.spec.js
@@ -24,43 +24,25 @@ describe("given I'm at the relative url 'items/create'", () => {
 
     describe("then the image preview element", () => {
         it("has no value in the 'src' attribute", () => {
-            expect(
-                createItemPage
-                    .imagePreviewContainer
-                    .image
-                    .getAttribute(constants.SRC_ATTRIBUTE)
-            ).toEqual("");
+            expect(createItemPage.imagePreviewContainer.image.getAttribute(constants.SRC_ATTRIBUTE))
+                .toEqual("");
         });
 
         it("doesn't render while the image URL filed is empty", () => {
-            expect(
-                createItemPage
-                    .imagePreviewContainer
-                    .image
-                    .isDisplayed()
-            ).not.toBe(true);
+            expect(createItemPage.imagePreviewContainer.image.isDisplayed()).not.toBe(true);
         });
     });
 
     describe("when I fill the image URL field with a valid image path", () => {
         // Multiple lines beforeEach arrow function
         beforeEach(() => {
-            helper.fillFieldWithTextWhenVisible(
-                createItemPage
-                    .form
-                    .imageUrlField,
-                constants.IMAGE_URL
-            );
+            helper.fillFieldWithTextWhenVisible(createItemPage.form.imageUrlField, constants.IMAGE_URL);
         });
 
         describe("then the image preview element", () => {
             it("uses the provided value in the 'src' attribute", () => {
-                expect(
-                    createItemPage
-                        .imagePreviewContainer
-                        .image
-                        .getAttribute(constants.SRC_ATTRIBUTE)
-                ).toEqual(constants.IMAGE_URL);
+                expect(createItemPage.imagePreviewContainer.image.getAttribute(constants.SRC_ATTRIBUTE))
+                    .toEqual(constants.IMAGE_URL);
             });
 
             it("is rendered based on the provided image url path", () => {
@@ -72,45 +54,28 @@ describe("given I'm at the relative url 'items/create'", () => {
     });
 
     describe("when I click the back button", () => {
-        it("then I'm redirected to the homepage", () => {
-            helper.clickWhenClickable(
-                createItemPage
-                    .form
-                    .backButton
-            );
+        beforeEach(() => helper.clickWhenClickable(createItemPage.form.backButton));
 
+        it("then I'm redirected to the homepage", () => {
             helper.waitForUrlToBeEqualToExpectedUrl(browser.baseUrl);
         });
     });
 
     describe("when I click the header's logo", () => {
-        it("then I'm redirected to the homepage", () => {
-            helper.clickWhenClickable(header.logo);
+        beforeEach(() => helper.clickWhenClickable(header.logo));
 
+        it("then I'm redirected to the homepage", () => {
             helper.waitForUrlToBeEqualToExpectedUrl(browser.baseUrl);
         });
     });
 
     describe("when I submit the form without filling any field", () => {
-        it("then three error messages are shown, one for each of the mandatory fields", () => {
-            helper.clickWhenClickable(
-                createItemPage
-                    .form
-                    .createButton
-            );
-            helper.waitForElementVisibility(
-                createItemPage
-                    .form
-                    .errors
-                    .last()
-            );
+        beforeEach(() => helper.clickWhenClickable(createItemPage.form.createButton));
 
-            expect(
-                createItemPage
-                    .form
-                    .errors
-                    .count()
-            ).toBe(3);
+        it("then three error messages are shown, one for each of the mandatory fields", () => {
+            helper.waitForElementVisibility(createItemPage.form.errors.last());
+
+            expect(createItemPage.form.errors.count()).toBe(3);
         });
     });
 
@@ -121,20 +86,11 @@ describe("given I'm at the relative url 'items/create'", () => {
             imageUrl: "http://localhost:4001/img/logo.svg"
         };
 
-        beforeEach(() => {
-            createItemPage
-                .form
-                .fillFormWithDataAndSubmit(newItem);
-        });
+        beforeEach(() => createItemPage.form.fillFormWithDataAndSubmit(newItem));
 
         it("then I'm redirected to the home page and an item with the provided title is displayed", () => {
             helper.waitForUrlToBeEqualToExpectedUrl(browser.baseUrl);
-            helper.waitForTextToBePresentInElement(
-                homePage
-                    .items
-                    .container,
-                newItem.title
-            );
+            helper.waitForTextToBePresentInElement(homePage.items.container, newItem.title);
         });
     });
 });

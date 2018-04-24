38e36ee550719623d35b8fc5c63130642dd30c9f
wlsf82 <wlsf82@gmail.com>
2018-04-24 03:27:43 +0200

    add e2e test case for view item page

---
+++ test/e2e/specs/item.spec.js
@@ -0,0 +1,29 @@
+const helper = require("protractor-helper");
+
+const HomePage = require("../page-objects/home");
+
+describe("given I'm at the home page", () => {
+    const homePage = new HomePage();
+
+    beforeEach(() => browser.get(homePage.relativeUrl));
+
+    describe("when I visit the relative URL of the first item in the home page", () => {
+        let firstItemRelativeUrl;
+
+        beforeEach(() => {
+            homePage.items.viewButtonOfFirstItem.getAttribute("href")
+                .then(hrefValue => {
+                    firstItemRelativeUrl = hrefValue.replace(browser.baseUrl, "/");
+                    browser.get(firstItemRelativeUrl);
+                });
+        });
+
+        it("then I see a button to go to the update item page", () => {
+            const updateButton = element(by.css(`a[href='${firstItemRelativeUrl}/update']`));
+
+            helper.waitForElementVisibility(updateButton);
+        });
+    })
+
+
+});
